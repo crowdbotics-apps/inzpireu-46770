028daf23b19fd3d0ca31e462d2c5804b30acfc19
Crowdbotics <team@crowdbotics.com>
2024-03-03 01:27:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "inzpireu-46770",
+  url: "https://inzpireu-46770.botics.co",
+  api: "https://inzpireu-46770.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
